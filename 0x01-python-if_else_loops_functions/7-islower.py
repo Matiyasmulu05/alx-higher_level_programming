052501c85921b1b961f1c56df546317ae4caa8f3
Matiyasmulu05 <matiyasmulu69@gmail.com>
2023-11-28 10:47:26 -0800

    M

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-[2;2R[>77;30604;0c]10;rgb:bfbf/bfbf/bfbf]11;rgb:0000/0000/0000    return ord("a") <= ord(c) <= ord("z")
+    return ord("a") <= ord(c) <= ord("z")
